5dc112ac83e14664653e35e7b64ccd35324ba632
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-05-21 21:54:47 -0600

    Add laptop details to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop = {
+  shape: "rectangle",
+  color: "silver",
+  cpu_hertz: 2000,
+  number_of_keys: 80,
+  is_macbook: true,
+  has_wifi: true,
+  users: ["Kaity", "Andy", "Valerie"],
+  passwords: ["1234", "password", "1111"]
+}
