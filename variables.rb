e55c0c494ba9be67862db50c9e671df9e730cbe8
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-05-21 21:47:26 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,15 @@
+animal = "dog"
+is_cuddly = true
+animal = "cat"
+num_8 = 2
+num_3 = 10
+
+quotient = num_3 / num_8
+
+
+favorite_films = ["Happy Gilmore", "Pulp Fiction", "Inception"]
+
+favorite_films.drop(1)
+
+
+puts animal.length
